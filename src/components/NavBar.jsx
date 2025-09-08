c47e863bb2c1ae82e6e73957910bbf9257842732
AJ <terryant64@gmail.com>
2025-09-07 21:36:17 -0400

    small changes'

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -24,8 +24,10 @@ export default function NavBar()
   const TRACKS = useMemo(() => [
     "Space Junk Road.mp3",
     "Stardust Sppedway (G).mp3",
+    "Simpsons Hotline.mp3",
     "Beneath the Mask -rain-.mp3",
     "Darkness Time.mp3",
+    "Fresh (Chill Mix).mp3",
     "Nintendo (Remix).mp3",
     "how to sleep.mp3",
   ], []);
